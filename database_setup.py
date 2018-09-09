564bbd3aae702acfccc4deb72e652a876d8a83d2
Rina Lingad <pixie.czar@gmail.com>
2018-09-09 15:41:32 +1200

    Add users to the database

--- database_setup.py
+++ database_setup.py
@@ -8,11 +8,24 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
 
     name = Column(String(250), nullable = False)
     id = Column(Integer, primary_key = True)
+    address = Column(String(250), nullable = False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -32,6 +45,8 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
